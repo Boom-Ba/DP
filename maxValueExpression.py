208dc9e00a33b1250f4ae7d2371088dee1da840a
Boom-Ba <70102254+Boom-Ba@users.noreply.github.com>
2022-01-04 21:59:34 -0500

    Create maxValueExpression.py

---
+++ maxValueExpression.py
@@ -0,0 +1,24 @@
+import sys
+#given an array, to find the maximum value of expression A[i]-A[j]+A[k]-A[w] 
+#for all i>j>k>w, 
+#DP, time: n
+def maxValueExpression(arr,n):
+  if n<4:
+    return -1
+  first=[-sys.maxsize]*(n+1)
+  for i in range(n-1,-1,-1):
+    first[i] =max(arr[i],first[i+1])
+  print(first)
+  second=[-sys.maxsize]*n
+  for i in range(n-2,-1,-1):
+    second[i] =max(second[i+1],first[i]-arr[i])
+  third =[-sys.maxsize]*(n-1)
+  for i in range(n-3,-1,-1):
+    third[i] =max(third[i+1],second[i]+arr[i])
+  last =[-sys.maxsize]*(n-2)
+  for i in range(n-4,-1,-1):
+    last[i] =max(last[i+1],third[i]-arr[i])
+  return last[0]
+arr=[3,9,10,1,30,40]
+res=maxValueExpression(arr,len(arr))
+print(res)
